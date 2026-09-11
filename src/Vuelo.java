8171ac59ece0478af5cd6771d8bbd4923e485b13
JULIANPARDO2201 <julian.pardo.r2007@gmail.com>
2026-09-11 15:58:10 -0500

    Subir codigo actualizado

--- src/Vuelo.java
+++ src/Vuelo.java
@@ -27,13 +27,54 @@ public class Vuelo {
         }
     }
 
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
     public String getNumero() {
         return numero;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
     }
 
     public String getOrigen() {
         return origen;
     }
 
-    
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void mostrarInfo(){
+        System.out.println("-------------Vuelo "+numero+"--------------");
+        System.out.println("Ruta: " + origen + " -> "+ destino);
+        System.out.println("Capacidad: "+capacidadMaxima+" asientos");
+    }
+
+    public void mostarAsiento(){
+        System.out.println("Estado de asientos - Vuelo "+numero+": ");
+        for(Asiento a : asientos){
+            a.mostrarEstado();
+        }
+    }
+
+    public void embarcar(String codigoAsiento){
+        for(Asiento a : asientos){
+            if(a.getCodigo().equals(codigoAsiento)){
+                a.ocupar();
+                return;
+            }
+        }
+        System.out.println("Asiento " + codigoAsiento + "no encontrado en el vuelo "+numero+".");
+    }
+
+    public void desembarcar(String codigoAsiento){
+        
+    }
 }
